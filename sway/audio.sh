a58beef317498dfd0170ae6ce9ea5a33b00532b8
Elbert Oh <ellokri89@gmail.com>
2024-05-19 14:36:13 -0400

    update sway config

--- sway/audio.sh
+++ sway/audio.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-current_vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -c 9,11,12)
-if [[ $current_vol == 0* ]]; then
-	current_vol=$(echo $current_vol | cut -c 2,3)
-fi
+current_vol=""
+function get_vol() {
+	local vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -c 9,11,12)
+	if [[ $vol == 0* ]]; then
+		vol=$(echo $vol | cut -c 2,3)
+	fi
+	echo $vol
+}
+
+current_vol=$(get_vol)
+while [ "$current_vol" == "" ]; do
+	sleep 0.2
+	current_vol=$(get_vol)
+done
+
 echo "  ${current_vol}%"
